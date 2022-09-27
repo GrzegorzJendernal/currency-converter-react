41e4a5b6dc6b4e7c2fe4b031a88b10439eb5b204
GrzegorzJendernal <g.jendernal@gmail.com>
2022-09-27 19:28:40 +0200

    added Header and Footer modules

---
+++ src/Footer/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+const Footer = () => (
+    <footer className="footer">Kurs walut na podstawie: Tabela nr 153/A/NBP/2022 z dnia 2022-08-09 oraz Tabela nr
+        031/B/NBP/2022 z dnia
+        2022-08-03
+    </footer>
+);
+
+export default Footer; 
