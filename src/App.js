d5f46d64b620631d4be8c0921e07bf97d6d7210b
GrzegorzJendernal <g.jendernal@gmail.com>
2022-09-28 15:32:40 +0200

    added Result module without interaction

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from "./Header";
 import Footer from "./Footer";
 import Form from "./Form";
+import Result from "./Result";
 
 function App() {
   return (
     <>
       <Header title="Kalkulator walutowy" />
       <Form />
+      <Result />
       <Footer />
     </>
   );
