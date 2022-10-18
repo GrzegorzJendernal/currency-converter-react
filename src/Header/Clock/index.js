18201f7b090da4280917f31d68e254e3b16ac2ec
GrzegorzJendernal <g.jendernal@gmail.com>
2022-10-18 19:02:08 +0200

    moved render functions to Clock module

--- src/Header/Clock/index.js
+++ src/Header/Clock/index.js
@@ -2,10 +2,20 @@ import { useCurrentDate } from "./useCurrentDate";
 import { StyledClock } from "./styled";
 
 const Clock = () => {
-    const {
-        dateToRender,
-        timeToRender,
-    } = useCurrentDate();
+    const myDate = useCurrentDate();
+
+    const dateToRender = myDate.toLocaleDateString(undefined, {
+        month: "long",
+        weekday: "long",
+        day: "numeric",
+        year: "numeric",
+    });
+
+    const timeToRender = myDate.toLocaleTimeString(undefined, {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+    });
 
     return (
         <StyledClock>
--- src/Header/Clock/useCurrentDate.js
+++ src/Header/Clock/useCurrentDate.js
@@ -3,19 +3,6 @@ import { useState, useEffect } from "react";
 export const useCurrentDate = () => {
     const [myDate, setMyDate] = useState(new Date());
 
-    const dateToRender = myDate.toLocaleDateString(undefined, {
-        month: "long",
-        weekday: "long",
-        day: "numeric",
-        year: "numeric",
-    });
-
-    const timeToRender = myDate.toLocaleTimeString(undefined, {
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-    });
-
     useEffect(() => {
         const intervalId = setInterval(() => {
             setMyDate(new Date())
@@ -26,8 +13,5 @@ export const useCurrentDate = () => {
         };
     }, []);
 
-    return {
-        dateToRender,
-        timeToRender,
-    }
+    return { myDate };
 };
