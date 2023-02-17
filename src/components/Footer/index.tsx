ee6357b7bac78833d8564bdb80b6cd264e4775bc
GrzegorzJendernal <g.jendernal@gmail.com>
2023-02-17 13:55:07 +0100

    actualised Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { StyledFooter } from "./styled";
 import { Rates } from "../../types/data";
 
 const Footer = () => {
-	const {data} = useQuery<Rates>("rates");
+	const {data} = useQuery("rates") as Rates;
 	if (!data) return (<></>);
 	return (
 		<StyledFooter>Kurs walut pobrany z Europejskiego Banku Centralnego, aktualny na dzień: {data.date}
